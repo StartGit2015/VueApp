0779880eed223e6c82f3c4acc06438c19d5bf166
VueApp <XiaoZhongYan@outlook.com>
2017-05-22 20:57:41 +0800

    改改

--- Y.Core/WinForm/FormEx/BaseForm/BaseForm.Designer.cs
+++ Y.Core/WinForm/FormEx/BaseForm/BaseForm.Designer.cs
@@ -34,6 +34,7 @@
       // 
       this.ClientSize = new System.Drawing.Size(354, 309);
       this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
       this.Name = "BaseForm";
       this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
       this.ResumeLayout(false);
--- Y.Core/WinForm/FormEx/MainForm/MainForm.cs
+++ Y.Core/WinForm/FormEx/MainForm/MainForm.cs
@@ -398,9 +398,10 @@ namespace Y.Core.WinForm.FormEx
       // MainForm
       // 
       this.ClientSize = new System.Drawing.Size(761, 507);
+      this.CornerRadius = 4;
+      this.IsMdiContainer = true;
       this.Name = "MainForm";
       this.ResumeLayout(false);
-      this.IsMdiContainer = true;
 
     }
 
